938a04f1b85556ac52dd38aca660336fe5f7f21c
Bibek Regmi <bibek.regmi@swifttech.com>
2023-09-11 17:15:23 +0545

    last today office

--- server.js
+++ server.js
@@ -1,9 +1,42 @@
-const express = require('express')
-const dotenv= require('dotenv').config()
-const app= express();
-app.use(express.json());
+const express = require('express');
+const dotenv= require('dotenv')
+const grpc = require('@grpc/grpc-js')
+const protoLoader= require('@grpc/proto-loader')
+dotenv.config()
+app=  express();
 
-const port= process.env.PORT
-app.listen(port,()=>{
-    console.log(`Server listening at port ${port}`);
+const packageDefinition = protoLoader.loadSync('./proto/simpleCrud.rpc.proto', {
+    keepCase: true,
+    longs: 'string',
+    defaults: true,
+  })
+
+app.use(express.json())
+const server= new grpc.Server()
+
+
+const simpleProto = grpc.loadPackageDefinition(packageDefinition)
+
+const simpleServiceA = require('./controllers/index');
+
+server.addService(simpleProto.example.simpleCrud.rpc.simpleCrudService.service,{
+    create:simpleServiceA.createStudents,
+    read:simpleServiceA.readStudents,
+    readAll:simpleServiceA.readAllStudents,
+    update:simpleServiceA.updateStudents,
+    delete:simpleServiceA.deleteStudents
 })
+
+const port = process.env.PORT;
+server.bindAsync(
+    `${process.env.GRPC_HOST}:${port}`,
+    grpc.ServerCredentials.createInsecure(),
+    (err,port) => {
+      if (err) {
+        console.error(`Server error: ${err.message}`);
+      } else {
+        console.log(`Server bound on port: ${port}`);
+        server.start();
+      }
+    }
+  );
