4a1e5305e55ee71546087c3ca5017d5e33abe231
Bibek Regmi <bibek.regmi@swifttech.com>
2023-09-11 15:16:44 +0545

    commit server start

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-ok
+const express = require('express')
+const dotenv= require('dotenv').config()
+const app= express();
+app.use(express.json());
+
+const port= process.env.PORT
+app.listen(port,()=>{
+    console.log(`Server listening at port ${port}`);
+})
